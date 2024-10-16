54d427f7ec3cda82da74f20ab9afb101ac8151bf
arion aranda <arioncronos15s@gmail.com>
2024-10-15 20:40:07 -0600

    Se rediseñó la pantalla de ChangePwdScreen agregando un encabezado con fondo, un logo y  mi nombre, agrege la funcionalidad de cierre de sesión con el icono de 'Logout' .

--- app/src/main/java/com/example/a512lasalleapp/ui/screens/ChangePwdScreen.kt
+++ app/src/main/java/com/example/a512lasalleapp/ui/screens/ChangePwdScreen.kt
@@ -1,29 +1,86 @@
 package com.example.a512lasalleapp.ui.screens
 
+import androidx.compose.material.icons.Icons
+import com.example.a512lasalleapp.ui.utils.Logout
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.rememberNavController
-import com.example.a512lasalleapp.ui.components.ScreenTemplate
+import androidx.compose.ui.unit.sp
+import com.example.a512lasalleapp.R
 import com.example.a512lasalleapp.ui.theme._512LaSalleAppTheme
 
 @Composable
 fun ChangePwdScreen(innerPadding: PaddingValues) {
-    ScreenTemplate(innerPadding = innerPadding, header = {
-        Text(text = "Cambiar Contraseña", style = MaterialTheme.typography.headlineMedium)
-    }, body = {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+        // Header con imagen de fondo
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(bottomStart = 60.dp, bottomEnd = 60.dp))
+                .height(270.dp)
+                .background(MaterialTheme.colorScheme.primary)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.background),
+                contentDescription = "Background Image",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .offset(y = 70.dp)
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Cambio de Contraseña",
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Icon(
+                    imageVector = Logout,
+                    contentDescription = "Logout",
+                    modifier = Modifier
+                        .size(45.dp)
+                        .clickable {
+                            Log.i("HomeScreen", "Cerrando sesión")
+                        },
+                    tint = Color.White
+                )
+            }
+        }
+        // Body
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
-            Text("Pantalla de cambio de contraseña", style = MaterialTheme.typography.bodyLarge)
+            Text("Cambiar Contraseña:", style = MaterialTheme.typography.bodyLarge)
         }
-    })
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
